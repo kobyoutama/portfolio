34001cff6c5902b47a7654eddffba412b6b6e97d
Koby <koby.outama@gmail.com>
2023-06-06 14:29:58 -0700

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ import { Route, Routes} from "react-router-dom";
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
 import { NotFound } from './pages/NotFound/NotFound';
+import { Contact } from './pages/Contact/Contact';
 function App() {
   return (<>
     <GlobalNav/>
+    <Home/>
+    <About/>
+    <Contact/>
+    <NotFound/>
+    {/* This was for when it was multipage routing 
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/#/about" element={<About/>}/>
+      <Route path="/about" element={<About/>}/>
       <Route path='*' element={<NotFound/>}/>
-    </Routes>
-    <About/>
+    </Routes> */}
     </>
   );
 }
