4e004d3029510fcb0a9b470437f524fbec9b47c8
Koby <koby.outama@gmail.com>
2023-06-06 14:29:39 -0700

    added contact section

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import './Contact.css'
+import { GlobalNav } from "../../components/globalNav/globalNav";
+import { RandomTextEffect } from "../../components/randomTextEffect";
+
+export function Contact(){
+    return(
+        <div className="contact">
+            <h1>contact section</h1>    
+            <div className='about-desc'>Type Type</div>
+        </div>
+    )
+}
