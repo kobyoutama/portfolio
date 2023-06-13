e61bf64fb507b667081308175b864f4c2a62afbe
Koby <koby.outama@gmail.com>
2023-06-13 15:32:32 -0700

    added listener for resizing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,26 @@
 import './Home.css'
 import { RandomTextEffect } from "../../components/randomTextEffect";
+import { useEffect, useState } from 'react';
 
-export function Home(){
+interface HomeProp{
+    width:number
+}
+
+export function Home(props: HomeProp){
+    let defaultName = props.width<600 ?"Koby S. Outama":"Koby Shaisethah Outama";
+    const [name, setName] = useState(defaultName);
+    
+    useEffect(() => {
+        props.width < 600 ? 
+            setName("Koby S. Outama")
+            : setName("Koby Shaiseththah Outama");  
+    });
+
     return(
         <div className="Home" id='home'> 
         <main className='Home-main'>
-            <div className="Home-desc">
-            <RandomTextEffect>Hello! My Name Is</RandomTextEffect>
-            </div>
-            <RandomTextEffect>Koby Shaiseththah Outama</RandomTextEffect>
+            <RandomTextEffect className='Home-desc'>Hello! My Name Is</RandomTextEffect>
+            <RandomTextEffect text={name}>{defaultName}</RandomTextEffect>
             <div className="Home-desc">
             <RandomTextEffect>I am a UC Irvine graduate with a</RandomTextEffect>
             <RandomTextEffect speed={175}>B.S. IN</RandomTextEffect>
